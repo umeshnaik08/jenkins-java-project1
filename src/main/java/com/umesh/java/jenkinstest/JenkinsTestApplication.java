69b62330fbc50c8f2635ba8e8427c09afc4f4f5b
Umesh <umeshnaik2008@gmail.com>
2019-04-23 00:41:06 +0100

    commit1

--- src/main/java/com/umesh/java/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/umesh/java/jenkinstest/JenkinsTestApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsTestApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(JenkinsTestApplication.class, args);
+		System.out.println("Git commit test");
 	}
 
 }
